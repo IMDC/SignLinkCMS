fdce47b747af617c186b78f1b40619723432d57e
heidi <heidi@f4f87d3d-8582-40b1-ac76-6ca35fe44f46>
2008-04-24 16:19:55 +0000

    preferences

--- trunk/logout.php
+++ trunk/logout.php
@@ -7,6 +7,8 @@ unset($_SESSION['login']);
 unset($_SESSION['valid_user']);
 unset($_SESSION['member_id']);
 unset($_SESSION['is_admin']);
+unset($_SESSION['errors']);
+unset($_SESSION['notices']);
 
 $_SESSION['feedback'][] = 'Successfully logged out.';
 
