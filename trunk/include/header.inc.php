e58f9d569443e26496629a8b5d77992b4523d751
heidi <heidi@f4f87d3d-8582-40b1-ac76-6ca35fe44f46>
2009-01-26 21:40:46 +0000

    front login

--- trunk/include/header.inc.php
+++ trunk/include/header.inc.php
@@ -13,7 +13,7 @@
  
 </head>
 
-<ul id="menu-right">
+<!-- ul id="menu-right">
 	<li><a href="preferences.php"><img src="images/cog.png" alt="preferences" title="preferences" /></a></li>
 	<li><a href="help.php"><img src="images/help.png" alt="help" title="help" /></a></li>
 	<li>
@@ -24,25 +24,38 @@
 		echo '<a href="login.php"><img src="images/door_in.png" alt="login" title="login" /></a>'; 
 	}		
 	?>&nbsp;</li>
-</ul>
+</ul -->
 			
 <div id="container">
 
-	<div>
-		<div style="margin-top:5px;"><img src="images/fallback_forum_banner.png" alt="Signlink Studio Forum" /></div>
-
-		<div id="menu">
-		<?php		
-			$current_page = explode('/', $_SERVER['PHP_SELF']); 
-			$current_page = $current_page[count($current_page) - 1];
-		?>
-			<ul>					
-				<li><a href="index.php"><img src="images/house.png" alt="home" title="home" <?php if($current_page == 'index.php' || $current_page == 'page_view.php') { echo 'style="background-color: #cbdbef; border: 1px solid #7299C9;"'; } ?> /></a></li>
-				<li><a href="forums.php"><img src="images/group.png" alt="forums" title="forums" <?php if(in_array($current_page, $forum_pages)) { echo 'style="background-color: #cbdbef; border: 1px solid #7299C9;"'; } ?> /></a></li>	
-				<li><a href="vlogs.php"><img src="images/cup_edit.png" alt="vlogs" title="vlogs" <?php if($current_page == 'vlogs.php') { echo 'style="background-color: #cbdbef; border: 1px solid #7299C9;"'; } ?> /></a></li>				
-			</ul>
-		</div>
+	<div id="login" style="float:right; font-size:smaller; padding:10px;">
+		<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="form">
+		
+			<input type="hidden" name="f" value="<?php echo intval($_REQUEST['f']); ?>" />
+			<input type="hidden" name="p" value="<?php echo intval($_REQUEST['p']); ?>" />
+		
+			<div style="width:5em"><label for="login">Login:</label></div> <input name="login" type="text" id="login" value="<?php echo $_SERVER['login']; ?>" style="width:10em" />
+			<div style="width:5em"><label for="pswd">Password:</label></div> <input name="password" type="password" id="pswd" value="" style="width:10em" />
+			<input type="submit" name="submit" value="Submit" class="button" style="font-size:smaller;margin-left:1em" />
+		</form>
 	</div>
+
+	<div style="margin-top:5px;"><img src="images/fallback_forum_banner.png" alt="Signlink Studio Forum" /></div>
+
+	
+
+	<div id="menu" style="clear:both">
+	<?php		
+		$current_page = explode('/', $_SERVER['PHP_SELF']); 
+		$current_page = $current_page[count($current_page) - 1];
+	?>
+		<ul>					
+			<li><a href="index.php"><img src="images/house.png" alt="home" title="home" <?php if($current_page == 'index.php' || $current_page == 'page_view.php') { echo 'style="background-color: #cbdbef; border: 1px solid #7299C9;"'; } ?> /></a></li>
+			<li><a href="forums.php"><img src="images/group.png" alt="forums" title="forums" <?php if(in_array($current_page, $forum_pages)) { echo 'style="background-color: #cbdbef; border: 1px solid #7299C9;"'; } ?> /></a></li>	
+			<li><a href="vlogs.php"><img src="images/cup_edit.png" alt="vlogs" title="vlogs" <?php if($current_page == 'vlogs.php') { echo 'style="background-color: #cbdbef; border: 1px solid #7299C9;"'; } ?> /></a></li>				
+		</ul>
+	</div>
+
 
 	<div id="content">
 	<?php 
