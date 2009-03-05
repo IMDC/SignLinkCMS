48a91928b703e40c21e88ca0bcb84a6303d6859c
heidi <heidi@f4f87d3d-8582-40b1-ac76-6ca35fe44f46>
2009-03-05 15:10:44 +0000

    comments

--- trunk/vlog_entry_view.php
+++ trunk/vlog_entry_view.php
@@ -53,35 +53,65 @@ if (!$row = @mysql_fetch_assoc($result)) {
 		<?php  echo get_vlog_message($row['content'], $row['content_alt'], 'entries', $entry_id); ?>
 	</div>
 	<br style="clear:both" />
-</div>
-
-<div style="padding-left:5px; padding-right:5px;">
-	<a style="float:right;" href="vlog_comment.php?v=<?php echo $vlog_id.'&e='.$entry_id; ?>"><img src='images/comment_add.png' alt='Add comment' title='Add comment' /></a>
-	<h3>Comments</h3>	
-</div>
-<div style="padding:5px;">	
-	<?php
-	/* comments */
+	
+	<?php	
 	$sql = "SELECT * FROM vlogs_comments WHERE vlog_id=".$vlog_id." AND entry_id=".$entry_id." ORDER BY date DESC";
 	$result = mysql_query($sql, $db);
-	
-	if (mysql_num_rows($result)) {
-		while ($row = @mysql_fetch_assoc($result)) { 
-			echo '<div style="background-color:#efefef; margin-bottom:10px; padding:10px;">';
-			echo '<div style="float:left; text-align:center; width:100px;">';
-			get_avatar($row['member_id']);
-			echo '<div style="padding-bottom:10px;">'.get_login($row['member_id']).'</div>';
-			echo '</div>';
-			
-			echo '<div style="margin-left:110px;">';				
-			echo get_vlog_message($row['comment'], $row['comment_alt'], 'comments', $row['comment_id']);
-			echo '</div><br style="clear:both" />';		
-			echo '</div>';
+	if (@mysql_num_rows($result)) { ?>
+		<table class="manage">
+		<tr>
+			<th colspan="3"><a style="float:right" href="vlog_comment.php?v=<?php echo $vlog_id.'&e='.$entry_id; ?>"><img src='images/comment_add.png' alt='Add comment' title='Add comment' /></a> Comments</th>
+		</tr>
+		<?php 
+			while ($row = mysql_fetch_assoc($result)) { 
+				echo '<tr>';
+				if (!empty($row['comment'])) {
+					//the msg is plain text
+					$link = substr($row['comment'],0,30).'...';
+				} else {
+					//the msg is a file
+					$level = '';
+					$depth = substr_count(INCLUDE_PATH, '/');
+					for ($i=1; $i<$depth; $i++) {
+						$level .= "../";
+					}
+					
+					//get files
+					$dir_files = @scandir($level.'uploads/comments/'.$id.'/');
+		
+					//pick out the "message" file and check its extension
+					if (!empty($dir_files)) {
+						foreach ($dir_files as $dir_file) {
+							if (substr($dir_file,0, 7) == "message") {
+								$msg_file = $dir_file;
+								break;
+							}
+						}
+						$ext = end(explode('.',$msg_file));
+						if (in_array($ext, $filetypes_video)) {
+							$link = '<img src="images/film.png" alt="movie content" style="border:0px;" />';
+						} else if ($ext=="swf") {
+							$link = '<img src="images/television.png" alt="signlink content" style="border:0px;" />';
+						}
+					}
+				}
+				echo '<td><a href="vlog_comment_view.php?v='.$vlog_id.'&e='.$entry_id.'&c='.$row['comment_id'].'">'.$link.'</a></td>';
+				echo '<td style="text-align:center;">'.get_login($row['member_id']).'</td>'; 
+				?>
+				
+				<td style="text-align:center">
+					<?php echo date('M j Y, h:ia', strtotime($row['date'])); ?>
+				</td>
+			</tr>
+		<?php
 		}
 	} else {
 		echo "No comments.";
-	}	
-	?>
+	}
+	
+	?>	
+	
 </div>
+
 
 <?php require(INCLUDE_PATH.'footer.inc.php'); ?>
