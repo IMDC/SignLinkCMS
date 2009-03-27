0c82ac90f9d388693e37ee28180021d1c212371b
heidi <heidi@f4f87d3d-8582-40b1-ac76-6ca35fe44f46>
2009-03-27 17:39:51 +0000

    forum delete, vlog delete

--- admin/forum_delete.php
+++ admin/forum_delete.php
@@ -13,14 +13,41 @@ if (isset($_GET['t']) && !empty($_GET['t'])) {
 	$_SESSION['feedback'][] = 'Forum title deleted.';
 
 } else {
+
+	//delete all forum post files
+	$sql = "SELECT * FROM forums_posts WHERE forum_id=".$forum_id;
+	$result = mysql_query($sql, $db);
+	if (@mysql_num_rows($result)) { 
+		while ($row = mysql_fetch_assoc($result)) {
+			$post_path = '../'.UPLOAD_DIR.'posts/'.$row['post_id'].'/';
+			if (file_exists($post_path)) {
+				$dir_files = @scandir($post_path);			
+				foreach ($dir_files as $dir_file) {
+					unlink($post_path.$dir_file);
+				}
+				rmdir($post_path);
+			}	
+		}
+	}
+
 	//delete threads
-	//$sql = "SELECT * FROM forums_threads WHERE forum_id=".intval($_GET['fid']);
-	//$result = mysql_query($sql, $db);
+	$sql = "DELETE FROM forums_posts WHERE forum_id=".$forum_id;
+	$result = mysql_query($sql, $db);
 	
+	//delete forum files
+	$forum_path = '../'.UPLOAD_DIR.'forums/'.$forum_id.'/';
+	if (file_exists($forum_path)) {
+		$dir_files = @scandir($forum_path);			
+		foreach ($dir_files as $dir_file) {
+			unlink($forum_path.$dir_file);
+		}
+		rmdir($forum_path);
+	}
 
 	//delete forum
 	$sql = "DELETE FROM forums WHERE forum_id=".$forum_id;
 	$result = mysql_query($sql, $db);
+
 	$_SESSION['feedback'][] = 'Forum deleted.';
 }
 
