563a496243e532ad7d960a5ac2613b80029bce60
paul <paul@f4f87d3d-8582-40b1-ac76-6ca35fe44f46>
2011-07-16 01:18:15 +0000

    Code indent changes for readability, inval check added to $id parameter on call to get_content. Added a TODO note to get_content to return html for a tinymce textarea instead of plain text html (ugly)

--- branches/testCode/v2/include/lib/pages.inc.php
+++ branches/testCode/v2/include/lib/pages.inc.php
@@ -1,11 +1,12 @@
 <?php
 /* functions related to pages only */
 
-define('VIDEO_MSG_HEIGHT', '260');
 define('VIDEO_MSG_WIDTH', '320');
+define('VIDEO_MSG_HEIGHT', '260');
 
 function get_content($id) {
 	global $db, $filetypes_video, $filetypes_image;
+   $id = intval($id);
 	
 	$level = '';
 	$depth = substr_count(INCLUDE_PATH, '/');
@@ -19,88 +20,79 @@ function get_content($id) {
 	$result = mysqli_query($db, $sql);
 	if ($result) {
 
-		if (!$row = mysqli_fetch_assoc($result)) {
+      if (!$row = mysqli_fetch_assoc($result)) {
 			$content = "No message.";
 			return $content;
 		}		
 
 		if (!empty($row['content'])) {
 			//plain text
+         // TODO: replace this with a TinyMCE textarea and set the contents of that textarea to $row['content']
 			$content = nl2br($row['content']);
 			
-		} else {
-			//file
+		}
+      else {
+			// content is a file
 			
-			//get files
+			// get files
 			$dir_files = @scandir($content_path);
 
 			if(!empty($dir_files)) {
 				foreach ($dir_files as $dir_file) {
-					if (substr($dir_file,0, 7) == "message") {
+					if (substr($dir_file, 0, 7) == "message") {
 						$content_file = $dir_file;
 						break;
 					}
 				}
 
-				$ext = end(explode('.',$content_file));
+				$ext = strtolower(end(explode('.',$content_file)));
 				
 				if (in_array($ext, $filetypes_video)) {
-				// if a video file
+               // if a video file
                /*
-					$content = '<object classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B"
-					id="clip" codebase="http://www.apple.com/qtactivex/qtplugin.cab">
-						<param name="src" value="'.$content_path.$content_file.'"/>
-						<param name="autoplay" value="false"/>
-						<param name="controller" value="true"/>
-						<param name="scale" value="tofit"/>
-						<embed src="'.$content_path.$content_file.'" name="clip"
-						autoplay="false" controller="true" enablejavascript="true" scale="tofit"
-						alt="Quicktime ASL video"
-						pluginspage="http://www.apple.com/quicktime/download/"
-						style="float:left;" />
-					</object>';
+               $content = '<object classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B"
+               id="clip" codebase="http://www.apple.com/qtactivex/qtplugin.cab">
+                  <param name="src" value="'.$content_path.$content_file.'"/>
+                  <param name="autoplay" value="false"/>
+                  <param name="controller" value="true"/>
+                  <param name="scale" value="tofit"/>
+                  <embed src="'.$content_path.$content_file.'" name="clip"
+                  autoplay="false" controller="true" enablejavascript="true" scale="tofit"
+                  alt="Quicktime ASL video"
+                  pluginspage="http://www.apple.com/quicktime/download/"
+                  style="float:left;" />
+               </object>';
                */
-         if ( file_exists($content_path . "thumb_play.jpg") ) {
-            $thumbjpg = $content_path . "thumb_play.jpg";
-           }
-           else if ( file_exists($content_path . "thumb.jpg") ) {
-              $thumbjpg = $content_path . "thumb.jpg";
-           }
-           else {
-              $thumbjpg = "images/default_movie_icon.png";
-           }
-           
-						/*
-            <a  
-							 href="'.$content_path.$content_file.'"
-							 class = "flash_player_holder" 
-							 style="background-image:url(\''.$thumbjpg.'\');display:block;width:'.VIDEO_MSG_WIDTH.'px;height:'.VIDEO_MSG_HEIGHT.'px;margin-left:auto;margin-right:auto;text-align:center;"
-							 id="'.$content_path.'content">
-							 <img src="images/play_large.png" style="border:0 none;margin-top:'.((VIDEO_MSG_HEIGHT/2)-30).'px;width:60px;height:60px;margin-left:auto;margin-right:auto;" alt="'.$row[1].'" />
-						</a> 
-           */
-           
-           $content = ' 
-						<a  
-							 href="'.$content_path.$content_file.'"
-							 class = "flash_player_holder" 
-                      alt="'.$row[1].'" 
-							 style="width:'.VIDEO_MSG_WIDTH.'px;height:'.VIDEO_MSG_HEIGHT.'px;margin-left:auto;margin-right:auto;"  
-							 id="'.$content_path.'content">
-							 <img style="margin-left:-3px;" src="'.$thumbjpg.'" height="'.VIDEO_MSG_HEIGHT.'px" width="'.VIDEO_MSG_WIDTH.'px" alt="'.$row[1].'" />
-						</a>
-						<script type="text/javascript">
-							flowplayer("'.$content_path.'content", "flash/flowplayer-3.2.7.swf", {
-								clip: {
-                  url: \''.$content_path.$content_file.'\',
-                  autoPlay: true,
-                  autoBuffering: true,
-                },
-                plugins: {
-                  controls: conf.big  
-                }
-							});
-						</script>';
+               if ( file_exists($content_path . "thumb_play.jpg") ) {
+                  $thumbjpg = $content_path . "thumb_play.jpg";
+               }
+               else if ( file_exists($content_path . "thumb.jpg") ) {
+                  $thumbjpg = $content_path . "thumb.jpg";
+               }
+               else {
+                  $thumbjpg = IMAGE_FOLDER . "default_movie_icon.png";
+               }
+
+              $content = '<a  
+                            href="'.$content_path.$content_file.'"
+                            class = "flash_player_holder" 
+                            alt="'.$row[1].'" 
+                            style="width:'.VIDEO_MSG_WIDTH.'px;height:'.VIDEO_MSG_HEIGHT.'px;margin-left:auto;margin-right:auto;"  
+                            id="'.$content_path.'content">
+                            <img style="margin-left:-3px;" src="'.$thumbjpg.'" height="'.VIDEO_MSG_HEIGHT.'px" width="'.VIDEO_MSG_WIDTH.'px" alt="'.$row[1].'" />
+                          </a>
+                          <script type="text/javascript">
+                             flowplayer("'.$content_path.'content", "flash/flowplayer-3.2.7.swf", {
+                                clip: {
+                                   url: \''.$content_path.$content_file.'\',
+                                   autoPlay: true,
+                                   autoBuffering: true,
+                                },
+                                plugins: {
+                                   controls: conf.big  
+                                }
+                             });
+                          </script>';
 				}
 				else if (in_array($ext, $filetypes_image)) {
 				// if an image file
@@ -122,10 +114,11 @@ function get_content($id) {
 			}
 		}
 		return $content;
-	} else {
+	}
+   else {
 		echo 'No content.';
-		return;
-	}	
+      return;
+   }	
 }
 
 function populate_page($row, $title) {
@@ -139,10 +132,12 @@ function populate_page($row, $title) {
 	if (!empty($row['title'])) {
 		$_POST['subject'] = 'text';
 		$_POST['sub-text'] = $row['title'];
-	} else if (substr($title, 0, 4) == "<img") {
+	}
+   else if (substr($title, 0, 4) == "<img") {
 		$_POST['subject'] = 'image';
 		$_POST['isub-alt'] = $row['title_alt'];
-	} else if (substr($title, 0, 7) == "<object") {
+	}
+   else if (substr($title, 0, 7) == "<object") {
 		$_POST['subject'] = 'video';
 		$_POST['vsub-alt'] = $row['subject_alt'];
 	}
@@ -152,10 +147,12 @@ function populate_page($row, $title) {
 		if (!empty($row['content'])) {
 			$_POST['message'] = 'text';
 			$_POST['msg-text'] = $row['content'];
-		} else if (substr($title, 0, 4) == "<img") {
+		}
+      else if (substr($title, 0, 4) == "<img") {
 			$_POST['message'] = 'image';
 			$_POST['imsg-alt'] = $row['content_alt'];
-		} else if (substr($title, 0, 7) == "<object") {
+		}
+      else if (substr($title, 0, 7) == "<object") {
 			$_POST['message'] = 'video';
 			$_POST['vmsg-alt'] = $row['subject_alt'];
 		}
