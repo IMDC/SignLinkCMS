b175cf758ad8156bec61f4242da96f06daa677ba
dom <dom@f4f87d3d-8582-40b1-ac76-6ca35fe44f46>
2011-08-12 17:48:21 +0000

    Updated forum_posts_manage.php page, with edit and delete working. Updated admin/forum_post_delete.php to work with the delete function on the forum_posts_manage.php page. Created admin/forum_post_edit.php to handle editing of posts and titles in the forum_posts_manage.php page.
    Updated forum_post_delete.php for users (non-admin page). Users can now only remove the content of their post, not able to delete the entire post.
    Commented out a #post-msg-text class in iehacks.php. It seemed to have been breaking the forum_posts_manage.php.

--- branches/testCode/v2/admin/forum_post_delete.php
+++ branches/testCode/v2/admin/forum_post_delete.php
@@ -6,53 +6,62 @@ admin_authenticate();
 $forum_id = intval($_GET['f']);
 $post_id = intval($_GET['p']);
 
-if ($post_id) {
+if ($post_id) 
+{
 	//check if it has children
 	$sql = "SELECT post_id FROM forums_posts WHERE parent_id=".$post_id." AND forum_id=".$forum_id;
 	$result = mysqli_query($db, $sql);
 	$row = mysqli_fetch_assoc($result);
-	if (!empty($row)) {
-		$_SESSION['errors'][] = 'Cannot delete post as it has replies. You must delete the replies (child posts) first.';	
-	} else {	
-		//if it's a child, edit num_comments for parent.
-		if ($row['parent_id']) {
-			$sql = "UPDATE forums_posts SET num_comments=num_comments-1 WHERE post_id=".$post_id." AND forum_id=".$forum_id;
-			$result = mysqli_query($sql, $db);
-		}
-		
-		//delete forum post
-		$sql = "DELETE FROM forums_posts WHERE forum_id=".$forum_id." AND post_id=".$post_id;
-		$result = mysqli_query($db, $sql);
+        
+        // if it's a parent, delete children
+	if (!empty($row)) 
+        {
+                $sql = "DELETE FROM forums_posts WHERE parent_id=".$post_id." AND forum_id=".$forum_id;
+                $result = mysqli_query($db, $sql); 
+	}
+        //if it's a child, edit num_comments for parent.
+        else
+        {
+                $sql_nested = "SELECT parent_id FROM forums_posts WHERE post_id=" .$post_id;
+                $result_nested = mysqli_query($db, $sql_nested);
+                $row_nested = mysqli_fetch_assoc($result_nested);
+                $sql = "UPDATE forums_posts SET num_comments=num_comments-1 WHERE post_id =".intval($row_nested['parent_id'])." AND forum_id=".$forum_id;
+                $result = mysqli_query($db, $sql);
+        }
 
-		//delete from forums_views & forums_read
-		$sql = "DELETE FROM forums_views WHERE post_id=".$post_id;
-		$result = mysqli_query($db, $sql);		
-		
-		$sql = "DELETE FROM forums_read WHERE forum_id=".$forum_id." AND post_id=".$post_id;
-		$result = mysqli_query($db, $sql);			
-		
-		//delete post files
-		$level = '';
-		$depth = substr_count(INCLUDE_PATH, '/');
-		for ($i=1; $i<$depth; $i++) {
-			$level .= "../";
-		}		
-		
-		$post_path = $level.UPLOAD_DIR.'posts/'.$post_id.'/';
-		if (file_exists($post_path)) {
-			//delete files
-			$dir_files = @scandir($post_path);			
-			foreach ($dir_files as $dir_file) {
-				@unlink($post_path.$dir_file);
-			}
-			
-			//delete directory
-			@rmdir($post_path);
-		}		
-		
-		
-		$_SESSION['feedback'][] = 'Forum post deleted.';
-	}	
+        //delete forum post
+        $sql = "DELETE FROM forums_posts WHERE forum_id=".$forum_id." AND post_id=".$post_id;
+        $result = mysqli_query($db, $sql);
+
+        //delete from forums_views & forums_read
+        $sql = "DELETE FROM forums_views WHERE post_id=".$post_id;
+        $result = mysqli_query($db, $sql);		
+
+        $sql = "DELETE FROM forums_read WHERE forum_id=".$forum_id." AND post_id=".$post_id;
+        $result = mysqli_query($db, $sql);			
+
+        //delete post files
+        $level = '';
+        $depth = substr_count(INCLUDE_PATH, '/');
+        for ($i=1; $i<$depth; $i++) {
+                $level .= "../";
+        }		
+
+        $post_path = $level.UPLOAD_DIR.'posts/'.$post_id.'/';
+        if (file_exists($post_path)) {
+                //delete files
+                $dir_files = @scandir($post_path);			
+                foreach ($dir_files as $dir_file) {
+                        @unlink($post_path.$dir_file);
+                }
+
+                //delete directory
+                @rmdir($post_path);
+        }		
+
+
+        $_SESSION['feedback'][] = 'Forum post deleted.';
+	
 } else {
 	$_SESSION['errors'][] = 'No such post.';	
 }
--- branches/testCode/v2/admin/forum_posts_manage.php
+++ branches/testCode/v2/admin/forum_posts_manage.php
@@ -9,6 +9,12 @@ $forum_id = intval($_GET['f']);
 ?>
 
 <link href="../css/admin.css" rel="stylesheet" type="text/css"/>
+<style>
+   .quickViewLink {
+      left: -65px;
+      top: -30px;
+   }
+</style>
 
 <h2>Forum Posts</h2>
 
@@ -46,8 +52,8 @@ if (mysqli_num_rows($result))
                 $title = preg_replace('/img class="quickView" src="images/', 'img class="quickView" src="../images', $title);
                 $title = preg_replace('/"flash\/flowplayer/', '"../flash/flowplayer', $title);
                 
-                $manage = '<a href="" onclick="return confirm(\'Edit Post\')">Edit</a>
-                           <a href="forum_post_delete.php?f='.$forum_id.'&p='.$row['post_id'].'" onclick="return confirm(\'Are you sure you want to delete this post and all its contents?\')">Delete</a>';
+                $manage = '<a href="forum_post_edit.php?f='.$forum_id.'&p='.$row['post_id'].'&thread=1">Edit Title</a><br />
+                           <a href="forum_post_delete.php?f='.$forum_id.'&p='.$row['post_id'].'" onclick="return confirm(\'Are you sure you want to delete this post and all its contents?\')">Delete Post</a>';
                 
                 echo '<div class="post-row forum-posts-table-elem">';
                 echo '<div class="id-col sidebyside">     '. $row['post_id'] .'</div>';
@@ -59,7 +65,7 @@ if (mysqli_num_rows($result))
                 
                 echo '<div id="post-pane" class="post-replies">';
 
-                $sql_replies = "SELECT post_id FROM forums_posts WHERE forum_id=".$forum_id." AND (parent_id=".$row['post_id']." OR post_id=".$row['post_id'].") ORDER BY date ASC";
+                $sql_replies = "SELECT post_id, parent_id FROM forums_posts WHERE forum_id=".$forum_id." AND (parent_id=".$row['post_id']." OR post_id=".$row['post_id'].") ORDER BY date ASC";
                 $result_replies = mysqli_query($db, $sql_replies);
 
                 while($replies = mysqli_fetch_assoc($result_replies))
@@ -67,82 +73,51 @@ if (mysqli_num_rows($result))
                     $msg = get_message($replies['post_id']);  //returns array of poster, date, html-encoded message
             ?>
         
-<!--                <table>
-                  <thead>
-                  <tr>
-                    <th>ID</th>
-                    <th>Subject</th>
-                    <th>Author</th>
-                    <th>Date</th>
-                    <th>Manage</th>
-                  </tr>
-                  </thead>
-                  <tbody>-->
-        
-<!--   THIS IS THE TESTING CODE FOR SHOWN REPLIES IN THE POSTS (copied from forum_post_view...)    -->
-
         <div id="post">
-	<div id="post-info">
-		<div style="padding-bottom:5px;"><?php echo $msg[0]; ?></div>
-    <?php echo '<small>' . $msg[1] . '</small>';?>
-	</div>
+            <div id="post-info">
+                <div style="padding-bottom:5px;"><?php echo $msg[0]; ?></div>
+                <?php echo '<small>' . $msg[1] . '</small>';?>
+                <div style="padding:10px;">
+                    <p><?php echo '<a href="forum_post_edit.php?f='.$forum_id.'&p='.$replies['post_id'].'&thread=0">Edit Reply</a>'?><br />
+                    <a href="" onclick="return confirm('Edit Post')">Remove Reply</a><br />
+                    <?php echo '<a href="forum_post_delete.php?f='.$forum_id.'&p='.$replies['post_id'].'" onclick="return confirm(\'Are you sure you want to delete this reply?\')">Delete Reply</a></p>'?>
+                </div>
+            </div>
 
-	<div id="post-msg">
-<!--		<div style="text-align:right">
-		<ul>
-         <?php
-//            if ($_SESSION['login'] == $msg[0]) {
-//               echo "<li style='display:inline;padding:8px;'><a href='forum_post_edit.php?f=$forum_id&p=$post_id&parent=$parent_id'><img src='images/comment_edit.png' alt='Edit' title='Edit' /></a></li>";
-//               echo "<li style='display:inline;padding:8xp;'><a href='forum_post_delete.php?f=$forum_id&p=$post_id&parent=$parent_id&m=$_SESSION[member_id]'><img src='images/comment_delete.png' alt='Delete' title='Delete' /></a></li>";
-//            }		
-//            
-//            if (!$parent_id) { 
-////               echo "<li style='display:inline;padding:8px;'><a href='forum_post_create.php?f=$forum_id&p=$post_id'><img src='images/comment_rev.png' alt='Reply' title='Reply to this post!' /></a></li>";
-//                 echo "<li style='display:inline;padding:8px;'><a href='forum_post_create.php?f=$forum_id&p=$post_id'><img src='images/slscms-reply-icon-idea-small.png' alt='Reply' title='Reply to this post!' /></a></li>";
-//            } 
-            
-         ?>
-		</ul>
-		</div>-->
-		<div id="post-msg-text">
-      <?php //echo '<small>' . $msg[1] . '</small><br />';   
+            <div id="post-msg">
+                    <div id="post-msg-text">
+                
+                <?php
+                     if ($msg[4] == 1) {
+                        // MESSAGE IS TEXT
+                        echo '<div>'.htmlspecialchars_decode($msg[2]).'</div>';
+                        //echo '<div style="height:100%;width=100%;overflow:auto;clear:right;">'.html_entity_decode($msg[2]).'</div>';
+                     }
+                     // otherwise the message is a video, signlink or old image type
+                     else {
+                        // Fix for flowplayer embed code returned from call to get_message in forums.inc.php
+                        // flowplayer doesn't like the id of an element to begin with "id='../uploads"
+                        // so we change only the id of the anchor to get rid of the '../' and do not change the href absolute path to the video file
+                        $fixedoutput = preg_replace('/id="..\/uploads\/posts/', 'id="uploads/posts', $msg[2]);
+                        // fix for flowplayer embed code returned from call to get_message in forums.inc.php 
+                        // this fix matches the one above it and makes sure the id of the element on the page that flowplayer replaces 
+                        // matches with the previous change to the element id by removing the '../'
+                        $fixedoutput = preg_replace('/flowplayer\("..\/uploads\/posts/', 'flowplayer("uploads/posts', $fixedoutput);
+                        // fix for flowplayer embed code returned from call to get_message in forums.inc.php
+                        // relative path to the flowplayer swf file needs to be changed because we are in the admin directory and not the site root
+                        // TODO: change the signature of the get_message method to accept a parameter that indicates the folder level or path to the flowplayer include files
+                        $fixedoutput = preg_replace('/flash\/flowplayer/', '../flash/flowplayer', $fixedoutput);
+                        echo $fixedoutput;
+                     }
+                ?>
+                    </div>
+                    <br style="clear:both" />
 
-         if ($msg[4] == 1) {
-            // MESSAGE IS TEXT
-            echo '<div style="height:100%;width=100%;overflow:auto;clear:right;">'.htmlspecialchars_decode($msg[2]).'</div>';
-            //echo '<div style="height:100%;width=100%;overflow:auto;clear:right;">'.html_entity_decode($msg[2]).'</div>';
-         }
-         // otherwise the message is a video, signlink or old image type
-         else {
-            // Fix for flowplayer embed code returned from call to get_message in forums.inc.php
-            // flowplayer doesn't like the id of an element to begin with "id='../uploads"
-            // so we change only the id of the anchor to get rid of the '../' and do not change the href absolute path to the video file
-            $fixedoutput = preg_replace('/id="..\/uploads\/posts/', 'id="uploads/posts', $msg[2]);
-            // fix for flowplayer embed code returned from call to get_message in forums.inc.php 
-            // this fix matches the one above it and makes sure the id of the element on the page that flowplayer replaces 
-            // matches with the previous change to the element id by removing the '../'
-            $fixedoutput = preg_replace('/flowplayer\("..\/uploads\/posts/', 'flowplayer("uploads/posts', $fixedoutput);
-            // fix for flowplayer embed code returned from call to get_message in forums.inc.php
-            // relative path to the flowplayer swf file needs to be changed because we are in the admin directory and not the site root
-            // TODO: change the signature of the get_message method to accept a parameter that indicates the folder level or path to the flowplayer include files
-            $fixedoutput = preg_replace('/flash\/flowplayer/', '../flash/flowplayer', $fixedoutput);
-            echo $fixedoutput;
-         }
-		?>
-		</div>
-		<br style="clear:both" />
-
-	</div>
-	<br style="clear:both" />
+            </div>
+            <br style="clear:both" />
         </div>
-        
-<!--    END OF SHOWN REPLIES CODE    -->
-        
+              
             <?php
-//                echo '<div class="id-col sidebyside">     '. $row['post_id'] .'</div>';
-//                echo '<div class="subject-col sidebyside">'. $title .'</div>';
-//                echo '<div class="author-col sidebyside"> '. $row['login'] .'</div>';
-//                echo '<div class="date-col sidebyside">   '. $row['date'] .'</div>';
             
                 } // while replies
                 
@@ -175,9 +150,9 @@ if (mysqli_num_rows($result))
     });
     
     $("div.text_title").css('height', '67px');
-    $("#post-msg-text").css('height', '100px');
-    $("#post-msg-text").css('width', 'auto');
-    $("#post-msg").css('padding', '5px 5px 5px 5px');
+//    $("#post-msg-text").css('height', '100px');
+//    $("#post-msg-text").css('width', 'auto');
+//    $("#post-msg").css('padding', '5px 5px 5px 5px');
     
 </script>
 
