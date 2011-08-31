d401c8290aeb0e9b37d7e987f94f7d0ee78cc0b9
dom <dom@f4f87d3d-8582-40b1-ac76-6ca35fe44f46>
2011-08-31 21:37:01 +0000

    Dom's last commit!
    Finished: 
     - forum_posts_manage.php: finishing touches, formatting.
     - vlog_manage.php: added edit and delete links, display login id of vlog owner.
    
    In progress:
     - vlog_entry_manage.php: managing entries (posts) in a users vlog (like forum_posts_manage.php).
     - vlog_edit.php: created file (should be like forum_edit.php)
    
    Still Needed:
     - vlog_post_edit.php: editing entries and comments
     - vlog_post_delete.php: deleting entries and comments

--- v2/admin/vlog_entry_manage.php
+++ v2/admin/vlog_entry_manage.php
@@ -5,7 +5,8 @@ admin_authenticate();
 
 require(INCLUDE_PATH.'admin_header.inc.php'); 
 
-$forum_id = intval($_GET['f']);
+$vlog_id = intval($_GET['v']);
+
 ?>
 <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/jquery-ui.min.js"></script>
 <link href="../css/admin.css" rel="stylesheet" type="text/css"/>
@@ -16,7 +17,7 @@ $forum_id = intval($_GET['f']);
    }
 </style>
 
-<h2>Forum Posts</h2>
+<h2>Vlog Entries</h2>
 
 <div style="padding-bottom:8px;">
 <?php
@@ -25,17 +26,16 @@ $page_title = adminMediaPathFix($page_title);
 
 echo '</div>';
 
-//get forum posts
-$sql = "SELECT * FROM forums_posts WHERE forum_id=".$forum_id." AND parent_id='0' ORDER BY date DESC";
+//get vlog entries
+$sql = "SELECT * FROM vlogs_entries WHERE vlog_id=".$vlog_id." ORDER BY date DESC";
 $result = mysqli_query($db, $sql);
 
 if (mysqli_num_rows($result)) 
 { ?>
     <div class="forum-posts-table">
-        <div class="forum-posts-table-header forum-posts-table-elem">
+        <div class="forum-posts-table-header forum-posts-table-elem centeralign">
             <div class="id-col sidebyside">      ID      </div>
-            <div class="subject-col sidebyside"> Subject </div>
-            <div class="author-col sidebyside">  Author  </div>
+            <div class="subject-col sidebyside"> Title   </div>
             <div class="date-col sidebyside">    Date    </div>
             <div class="manage-col sidebyside">  Manage  </div>
         </div>
@@ -43,36 +43,62 @@ if (mysqli_num_rows($result))
         <?php
             while($row = mysqli_fetch_assoc($result))
             {
-                $title = get_title('post', $row['post_id'], 'small');
+                $title = get_title('entry', $row['entry_id'], 'small');
                 $title = adminMediaPathFix($title);
                 
-                $manage = '<a href="forum_post_edit.php?f='.$forum_id.'&p='.$row['post_id'].'&thread=1">Edit Title</a><br />
-                           <a href="forum_post_delete.php?f='.$forum_id.'&p='.$row['post_id'].'" onclick="return confirm(\'Are you sure you want to delete this post and all its contents?\')">Delete Post</a>';
+                $manage = '<a href="vlog_post_edit.php?v='.$vlog_id.'&e='.$row['entry_id'].'&c=0">Edit Title</a><br />
+                           <a href="vlog_post_delete.php?v='.$vlog_id.'&e='.$row['entry_id'].'&c=0" onclick="return confirm(\'Are you sure you want to delete this vlog entry and all its comments?\')">Delete Entry</a>';
                 
                 echo '<div class="post-row forum-posts-table-elem">';
-                echo '<div class="id-col sidebyside">     '. $row['post_id'] .'</div>';
+                echo '<div class="id-col sidebyside">     '. $row['entry_id'] .'</div>';
                 echo '<div class="subject-col sidebyside">'. $title .'</div>';
-                echo '<div class="author-col sidebyside"> '. $row['login'] .'</div>';
                 echo '<div class="date-col sidebyside">   '. $row['date'] .'</div>';
                 echo '<div class="manage-col sidebyside">'. $manage .'</div>';
                 echo '</div>';
                 
                 echo '<div id="post-pane" class="post-replies">';
+                
+                ?>
+                <div id="post-main">
+                    <div id="post-info">
+                        <div style="padding:10px;">
+                            <p><?php 
+                            echo '<a href="vlog_post_edit.php?f='.$forum_id.'&p='.$replies['post_id'].'&thread=0">Edit Entry</a><br />';
+                            echo '<a href="vlog_post_delete.php?f='.$forum_id.'&p='.$replies['post_id'].'&del=0" onclick="return confirm(\'Are you sure you want to remove the contents of this reply?\')">Remove Reply</a><br />';
+                            ?></p>
+                        </div>
+                    </div>
+                    
+                    <div id="post-msg">
+                        <div id="post-msg-text">
 
+                        <?php
+//                        echo get_vlog_message($row['content'], $row['content_alt'], 'entries', $row['entry_id']);
+//                            if ($msg[4] == 1) { // MESSAGE IS TEXT
+//                                echo '<div>'.htmlspecialchars_decode($msg[2]).'</div>';
+//                            } 
+//                            else { // otherwise the message is a video, signlink or old image type
+//                                echo adminMediaPathFix($msg[2]);
+//                            }
+                        ?>
+<br /><br style="clear:both" />
+                        </div> <br style="clear:both" />
+                    </div> <br style="clear:both" />
+                </div>
+                <?php
+                
                 $sql_replies = "SELECT post_id, parent_id FROM forums_posts WHERE forum_id=".$forum_id." AND (parent_id=".$row['post_id']." OR post_id=".$row['post_id'].") ORDER BY date ASC";
                 $result_replies = mysqli_query($db, $sql_replies);
 
+/*============================================================================*/
+                
                 while($replies = mysqli_fetch_assoc($result_replies))
                 {
-                    $msg = get_message($replies['post_id']);  //returns array of poster, date, html-encoded message
+                    $msg = get_message($replies['entry_id']);  //returns array of poster, date, html-encoded message
                 ?>
         
-            <?php if($replies['post_id'] == $row['post_id']){ ?>
-            <div id="post-main">
-            <?php } else { ?>
+
             <div id="post">
-            <?php } ?>
-            
                 <div id="post-info">
                     <div style="padding-bottom:5px;"><?php echo $msg[0]; ?></div>
                     <?php echo '<small>' . $msg[1] . '</small>';?>
